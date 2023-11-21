8438117566d5f2ae35cd3a114c7f4434759b0e01
Dilan-Wijesinghe <dilan.wijesinghe0@gmail.com>
2023-11-21 11:19:04 -0600

    Added in changes to let the mouse follow trajectories from an Episode. Cleaned up some of the codeand began implementation of occlusion spawning

--- TCPTest/ControllerServerTest.py
+++ TCPTest/ControllerServerTest.py
@@ -1,14 +1,28 @@
 from json_cpp import *
 import tcp_messages as tcp
-from cellworld import Location, Step, Episode
+from cellworld import Location, Step, Episode, World
 from time import sleep
 
+# https://drive.google.com/drive/u/1/folders/1SJ0CZyrv3_Fax3SkkDf42P43zVBP3Ful
+
 # --- Load an Experiment ---
+occlusions = "21_05"
+world = World.get_from_parameters_names('hexagonal', "canonical", occlusions)
+# print(world.cells)
+vr_occlusion_locations = []
+print(len(world.implementation.cell_locations))
+# for cell in world.implementation.cell_locations:
+    # if(cell["occluded"]):
+    # print(cell)
+    # vr_occlusion_locations.append(cell["location"])
 epi = Episode.load_from_file("PEEK_20230201_1340_FMM13_21_05_RT3_episode_000.json")
+# epi = Episode.load_from_file("PEEK_20230131_1320_FMM13_21_05_RT2_episode_001.json")
 traj_obj = epi.trajectories.split_by_agent()
 prey_traj = traj_obj['prey']
+pred_traj = traj_obj['predator']
 # print(prey_traj)
 print(len(prey_traj))
+print(len(pred_traj))
 
 # for step in prey_traj:
 #     print(f"Step {step}")
@@ -22,11 +36,15 @@ class MyService(tcp.MessageServer):
         self.router.add_route("test3", self.test3)
         self.router.add_route("stop_server", self.stop_service)
         self.router.add_route("set_destination", self.echo, JsonString)
+        self.router.add_route("get_world_implementation", self.add_cells)
         # self.router.add_route("send_again", self.send_broadcast)
 
     # @staticmethod
     # @json_parameters_function()
     # def accum(v: int, v2: int):
+    # For adding in the locations of the occlusions into the VR World
+    def add_cells(self):
+        return JsonString(world.cells)
 
     #     return v + v2
     def echo(self, msg):
@@ -83,7 +101,7 @@ predator_steps.append(Step(location = Location(1.0, 0.5)) )
 for i in range(0,len(prey_traj)):
     print(f"Broadcast: {i}")
     service.send_broadcast_prey(prey_traj[i])
-    # service.send_broadcast_pred(predator_steps[i])
+    service.send_broadcast_pred(pred_traj[i])
     sleep(0.01)
     
 service.join()
