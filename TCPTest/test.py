826f34fc4bd7e49b29a5756e9cb18f1fb70b6554
felixmaldonadoos <famaldonadoos@gmail.com>
2024-02-27 16:23:34 -0600

    update

--- TCPTest/test.py
+++ TCPTest/test.py
@@ -1,27 +1,4 @@
-import socket
+import cellworld_experiment_service as cwes
 
-def main():
-    # Create a TCP/IP socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Connect the socket to the server's address and port
-    server_address = ('127.0.0.1', 6000)
-    print(f"Connecting to {server_address[0]} port {server_address[1]}")
-    client_socket.connect(server_address)
-    print("here")
-    try:
-        # Continuously receive data from the server and print it
-        while True:
-            data = client_socket.recv(1024)
-            print(f"Received: {data.decode('utf-8')}")
-            if not data:
-                # If no data is received, break out of the loop
-                print("No data from server, closing connection.")
-                break
-    finally:
-        # Clean up the connection
-        client_socket.close()
-        print("Disconnected from server.")
-
-if __name__ == "__main__":
-    main()
+service = cwes.ExperimentService()
+print("s")
