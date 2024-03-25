17d774c83e93a55f6e52c5c459d4067046546a77
felixmaldonadoos <famaldonadoos@gmail.com>
2024-03-25 14:03:08 -0500

    updated new server!

--- src/BotEvadeClient.py
+++ src/BotEvadeClient.py
@@ -4,7 +4,7 @@ from time import sleep
 from cellworld import Location, Step, Episode, World
 import cellworld_experiment_service as ces
 from cellworld_tracking import TrackingClient as tc 
-
+import time 
 class TrackingServiceClient(tcp.MessageClient):
     def __init__(self):
         super().__init__()
@@ -31,50 +31,52 @@ class TrackingServiceClient(tcp.MessageClient):
 class ExperimentServiceClient(ces.ExperimentClient):
     def __init__(self):
         super().__init__()
-        
-        # self.set_request_time_out(100)
         self.router.add_route("predator_step",self.echo, JsonString)
         self.on_episode_started = self.on_episode_started_es
         self.port = 4540
 
     def on_episode_started_es(self, msg:ces.EpisodeStartedMessage):
-            print(f"[ES] Episode Started: {msg}")
-
+        print(f"[ES] Episode Started: {msg}")
+      
     def pre_start(self)->None:
         res = self.connect("127.0.0.1")
         print(f"[ES] connected")
         try:
-            response_start_experiment = self.start_experiment(suffix="test",prefix="test",world_configuration="hexagonal",world_implementation="canonical",
+            self.response_start_experiment = self.start_experiment(suffix="test",prefix="test",world_configuration="hexagonal",world_implementation="canonical",
                         occlusions="21_05", subject_name="alexander",duration=100,
                         rewards_cells=None,rewards_orientations=None)
-            print(f"[ES] Start experiment response: {response_start_experiment}")
+            print(f"[ES] Start experiment response: {self.response_start_experiment}")
         except TimeoutError:
             print("[ES] Timed out")
             return
-
         try:
-            response_start_episode = self.start_episode(experiment_name=response_start_experiment.experiment_name,
+            self.response_start_episode = self.start_episode(experiment_name=self.response_start_experiment.experiment_name,
                                         rewards_sequence=None)
-            print(f"[ES] Start episode response: {response_start_episode}")
+            print(f"[ES] Start episode response: {self.response_start_episode}")
         except TimeoutError:
             print("[ES] Start episode timed out")
             return
     
     def run(self)->None:
         self.pre_start()
-        # todo: start sending steps 
-    
+
+    def stop(self)->None:
+        print("stop")
+        print(self.finish_episode())
+        print("stop exp")
+        print(self.finish_experiment(self.response_start_experiment.experiment_name))
+
     def echo(self,msg)->None:
         print(f"Sent: {msg}")
 
+# run tracking service
 TrackingServiceClient().run()
-ExperimentServiceClient().run()
 
+# run experiment service 
+esc = ExperimentServiceClient()
+esc.run()
 
-"""
-1. start experiment -> Name
-2. start episode(Name)
-3. 
-
-
-"""
+# finish episode and then finish experiment 
+print("[LOG] Preparing to stop...")
+time.sleep(0.5)
+esc.stop()
--- src/Server.py
+++ src/Server.py
@@ -1,66 +1,134 @@
 import cellworld_experiment_service as ces
 import cellworld_tracking as ct
-import tcp_messages as tm
+import tcp_messages as tcp
 import cellworld as cw
+import sys
+import json
+import errno
 
-class BotEvadeServer():
+class Server:
     def __init__(self) -> None:
         print("=== BotEvade Server ===")
-        pass
-    def TrackingService(self):
-        def send_step(step: ces.Step)->None:
-            print(step.agent_name)
-            # call gabbie script and gabbie.agent_name + _step = pred_step ## not implemented yet
-            # step = self.process_step(step)
-            self.ts.broadcast_subscribed(ces.Message(step.agent_name + "_step", step))
+        self.tracking_service   = ServerTrackingService()
+        self.experiment_service = ServerExperimentService()
         
-        self.ts                     = ct.TrackingService()
-        self.ts.ip                  = "127.0.0.1"
-        self.ts.on_new_connection   = self.on_connection_ts
-        self.ts.send_step           = send_step
-        res                         = self.ts.start(port=4510)
-        print(f"[TS] Started: {res} at port {self.ts.port()}")
- 
-    def ExperimentService(self):
+    def start(self)->bool:
+        if not self.tracking_service.run():
+            self.log("ERROR! Failed to start Tracking Service.")
+            return False
+        if not self.experiment_service.run():
+            self.log("ERROR! Failed to start Experiment Service.")
+            return False
+        return True
     
-        self.es                     = ces.ExperimentService()
-        # self.es.router.add_route("start_experiment_vr",self.start_experiment_vr, ces.StartExperimentRequestVR)
-        self.es.on_new_connection   = self.on_connection_es
-        self.es.on_episode_started  = self.on_episode_started_es
+    def stop(self)->None:
+        self.experiment_service.stop()
+        self.tracking_service.stop()
+        self.exit()
+        return
+    
+    def exit(self)->None:
+        self.log("Exiting program")
+        sys.exit()
 
-        self.es.set_tracking_service_ip("127.0.0.1")
-        res = self.es.start()
-        print(f"[ES] Started: {res} at port {self.es.port()}")
-        self.es.join()
+    def log(self, msg, *args):
+        additional_info = ', '.join(str(arg) for arg in args)
+        if additional_info:
+            print(f"[Server] {msg}. Info: {additional_info}")
+        else:
+            print(f"[Server] {msg}.")
 
-            
+class ServerTrackingService(ct.TrackingService):
+    def __init__(self) -> None:
+        ct.TrackingService.__init__(self)
+        self.ip                  = "127.0.0.1"
+        self.on_new_connection   = self.on_connection_ts
+        self.send_step           = self.send_step_ts
+        
+    def run(self)->bool:
+        # print('here')
+        res = False
+        try:
+            res = self.start(port=4510)
+            self.log("Started service.",res)
+        except OSError:
+            self.log("lol server is still refreshing")
+        return res
+
+    def on_connection_ts(self,msg)->None:
+        self.log("New connection.",msg)
+    
     def process_step(self, step:ces.Step)->ces.Step:
-        print("processing")
+        # print("processing")
         step.agent_name = "predator"
         return step
-    def echo_ts(self,msg)->None:
-        # print(f"Received: {msg}")
-        return None
 
-    def on_episode_started_es(self, msg):
-        print(f"[ES] Episode Started: {msg}")
-
-    def on_connection_es(self, connection_id)->None:
-        print("[ES] New connection!")
-
-    def on_connection_ts(self, connection_id)->None:
-        print("[TS] New connection!")
+    def send_step_ts(self,step:ces.Step=None)->None:
+        print(step.agent_name)
+        # call gabbie script and gabbie.agent_name + _step = pred_step ## not implemented yet
+        step = self.process_step(step)
+        self.ts.broadcast_subscribed(ces.Message(step.agent_name + "_step", step))
+        return
     
-
-class ExperimentServiceVR(ces.ExperimentService()):
-    def __init__(self)->None:
+    def log(self, msg, *args):
+        additional_info = ', '.join(str(arg) for arg in args)
+        if additional_info:
+            print(f"[TS] {msg}. Info: {additional_info}")
+        else:
+            print(f"[TS] {msg}.")
+    
+    def echo(self,msg):
+        self.log("message received:",msg)
+        
+class ServerExperimentService(ces.ExperimentService):
+    def __init__(self):
+        super().__init__()
+        self.on_new_connection      = self.on_new_connection_es
+        self.on_experiment_started  = self.on_experiment_started_es
+        self.on_experiment_finished = self.on_experiment_finished_es
+        self.on_episode_started     = self.on_episode_started_es
+        self.on_episode_finished    = self.on_episode_finished_es
+        self.on_experiment_resumed  = self.on_experiment_resumed_es        
+        self.set_tracking_service_ip("127.0.0.1")
         pass
 
+    def run(self)->bool:
+        res = self.start()
+        self.log(f"Started server", res)
+        self.join()
+        print('after run ') ## never fires 
+        return res
+
+    def on_new_connection_es(self,msg:tcp.Connection = None)->None:
+        self.log("New connection", msg.state)
+
+    def on_episode_started_es(self,msg:ces.StartEpisodeRequest = None)->None:
+        self.log("episode started",msg)
+    
+    def on_experiment_started_es(self,msg:ces.StartExperimentResponse = None)->None:
+        self.log("Experiment started.",f"experiment name: {msg.experiment_name}")
+    
+    def on_experiment_resumed_es(self,msg:ces.ResumeExperimentRequest = None)->None:
+        self.log("Experiment resumed",
+                 f"experiment name: {msg.experiment_name}", 
+                 f"duration extension: {msg.duration_extension}")
+
+    def on_experiment_finished_es(self,msg:bool = None)->None:
+        self.log("Experiment finished.",msg)
+
+    def on_episode_finished_es(self,msg:bool = None)->None:
+        self.log("Episode finished.", msg)
+
+    def log(self, msg, *args):
+        additional_info = ', '.join(str(arg) for arg in args)
+        if additional_info:
+            print(f"[ES] {msg}. Info: {additional_info}")
+        else:
+            print(f"[ES] {msg}.")
+       
 
 def main():
-    s = BotEvadeServer()
-    s.TrackingService()
-    s.ExperimentService()
-
+    s = Server()
+    s.start()
 if __name__ == "__main__":
     main()
